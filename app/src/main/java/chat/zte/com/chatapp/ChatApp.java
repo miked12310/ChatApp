74f8811d7aa4dc2d00c2b992c1d6ac1f5d5d6f3c
qwerqqwewq <2725543850@qq.com>
2020-04-20 18:02:50 +0800

    1

---
+++ app/src/main/java/chat/zte/com/chatapp/ChatApp.java
@@ -0,0 +1,13 @@
+package chat.zte.com.chatapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ChatApp extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_chat_app);
+    }
+}
